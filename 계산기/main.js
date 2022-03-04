a801d49f9fd2fe80d0feb1a8ab65cabd34b3ec66
seyoungKim <sagekim6@gmail.com>
2022-03-04 22:28:52 +0900

    1st-HTML,CSS

---
+++ 계산기/main.js
@@ -0,0 +1,4 @@
+const display1El = document.querySelector('.display-1');
+const display2El = document.querySelector('.display-2');
+const tempResultEl = document.querySelector('.temp-result');
+console.log(display1El, display2El, tempResultEl);
